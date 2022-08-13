998d042e33f0a31c842898939577ff1305fc9479
Teo <btebaldi@deskpaper.com>
2022-08-13 11:47:53 -0300

    pca no r

--- R/Analise de PCA.R
+++ R/Analise de PCA.R
@@ -5,7 +5,9 @@
 rm(list = ls())
 
 library(readxl)
-
+library(dplyr)
+library(ggplot2)
+library(factoextra)
 
 
 db <- read_excel("../Simulacao GYORGY.xlsx", 
@@ -18,4 +20,46 @@ db <- read_excel("../Simulacao GYORGY.xlsx",
                  skip = 1)
 
 db$Data <- as.Date(db$Data)
-db
+
+pca.data <- db %>%
+  select(Alternativo, 
+         Inflacao, 
+         Internacional,
+         Multimercados,
+         Pos_fixado,
+         Pre_fixado,
+         Renda_Variavel,
+         Sem_Classificacao) %>% 
+  filter(row_number() > 1)
+
+
+res.pca <- prcomp(pca.data, scale = TRUE)
+
+
+fviz_eig(res.pca)
+
+
+fviz_pca_ind(res.pca,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
+
+
+
+fviz_pca_var(res.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
+
+
+
+fviz_pca_biplot(res.pca, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969")  # Individuals color
+
+
+res.pca$rotation
+res.pca$center
+res.pca$scale
+res.pca$x
+
+
+
